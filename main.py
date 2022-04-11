014fbeeb374f5b30b1c3a90904912cbf03cafd2a
Rafał Zalewski <zalfik@gmail.com>
2022-04-11 10:05:01 +0200

    Branch lmlp done

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 # import random
 
-from utilities import multidraw
+from utilities import multidraw, make_hits_dict, your_results
 
 my_coupon = [7, 18, 22, 26, 33, 49]
 draws_set = {}
-how_many = 0
 
-print('Hello! This is your coupon: ', my_coupon)
+print(f'Hello! This is your coupon: {my_coupon}')
 how_many = int(input('How many draws do you wanna make?\n'))
 
 multidraw(draws_set, how_many)
 print(draws_set)
+print('===========================================================')
+hits_dict = make_hits_dict(draws_set, my_coupon)
+print(your_results(hits_dict))
+
+
--- utilities.py
+++ utilities.py
@@ -2,36 +2,63 @@ import random
 
 
 # Class of coupons
-class Coupons:
+class Lottery:
     def __init__(self, date):
         # Attribute date will be used later... For now, it's just a number
         self.date = date
         self.single_draw = []
 
-    # Fill a coupon with random numbers
-    def fill_coupon(self):
-        numbers_per_coupon = 6
+    # Fills a coupon with random numbers
+    def fill(self):
+        numbers_per_draw = 6
         numbers_range = 49
-        while len(self.single_draw) < numbers_per_coupon:
+        while len(self.single_draw) < numbers_per_draw:
             number = random.randint(1, numbers_range)
             if not self.single_draw.count(number):
                 self.single_draw.append(number)
         self.single_draw.sort()
-# class Verifier(Coupons):
-#     def __init__(self):
-#         self.user_coupon = []
 
 
-# Creates a dictionary of >number< draws.
-def multidraw(coupon, number):
-    for i in range(len(coupon) + 1, len(coupon) + number + 1):
-        single_draw = Coupons(i)
-        single_draw.fill_coupon()
-        coupon[i] = single_draw.single_draw
+# Creates a dictionary of >number< draws. Adds new draws if not empty.
+def multidraw(draw_dict, number):
+    for i in range(len(draw_dict) + 1, len(draw_dict) + number + 1):
+        single_draw = Lottery(i)
+        single_draw.fill()
+        draw_dict[i] = single_draw.single_draw
 
 
-def count_hits(drawn, chosen, hit):
-    for i in chosen:
+# Counts hits in drawn from coupon
+def count_hits(drawn, coupon):
+    hits = 0
+    for i in coupon:
         if drawn.count(i):
-            hit += 1
-    return hit
+            hits += 1
+    return hits
+
+
+# Creates a dictionary where key is number of hits and value is a list of lottery numbers
+def make_hits_dict(draws_set, coupon):
+    hits_dict = {
+        '0': [],
+        '1': [],
+        '2': [],
+        '3': [],
+        '4': [],
+        '5': [],
+        '6': []
+    }
+    for lottery_number in draws_set:
+        hits = count_hits(draws_set[lottery_number], coupon)
+        hits_dict[str(hits)].append(lottery_number)
+    return hits_dict
+
+
+def your_results(hits_dict):
+    print(f'No hits in {len(hits_dict["0"])} draws\n'
+          f'One hit in {len(hits_dict["1"])} draws\n'
+          f'Two hits in {len(hits_dict["2"])} draws\n'
+          f'Tree hits in {len(hits_dict["3"])} draws\n'
+          f'Four hits in {len(hits_dict["4"])} draws\n'
+          f'Five hits in {len(hits_dict["5"])} draws\n'
+          f'Six hits in {len(hits_dict["6"])} draws\n')
+
